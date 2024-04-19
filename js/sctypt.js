3e89cdd741f65a2e0d25122702a37f68601b00a2
Vinicius <vsslviniciussousa@gmail.com>
2024-04-19 15:02:39 -0300

    Adição da section 'Portifólio'

--- js/sctypt.js
+++ js/sctypt.js
@@ -99,19 +99,55 @@ $(document).ready(function () {
             circleD.animate(1.0);
 
             stop = 1;
-        
+
         }
 
     });
 
     // Parallax
-    setTimeout(function() {
+    setTimeout(function () {
 
-        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
-        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
+        $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
     }, 250);
 
+    // Filtro do portifólio
+    $('.filter-btn').on('click', function () {
 
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes)
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes)
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+
+    }
 
 });
